4c419aced446b5ad326be1b86fa14c4f08812116
Paschal Ihenacho <ihenachopaschal@yahoo.com>
2020-09-03 00:50:20 -0500

    resume and contact page

--- src/components/contact.js
+++ src/components/contact.js
@@ -8,10 +8,10 @@ class Contact extends Component {
       <div className="contact-body">
         <Grid className="contact-grid">
           <Cell col={6}>
-            <h2>Paul Hanna</h2>
+            <h2>Paschal Ihenacho</h2>
             <img
-              src="https://cdn2.iconfinder.com/data/icons/avatar-2/512/Fred_man-512.png"
-              alt="avatar"
+              src="ProfileImage.png"
+              alt="ProfileImage"
               style={{height: '250px'}}
                />
              <p style={{ width: '75%', margin: 'auto', paddingTop: '1em'}}>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries</p>
